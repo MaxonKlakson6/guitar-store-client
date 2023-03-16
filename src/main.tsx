a9c6ab57cb0c6f1631c56af92c94b8a1b51e1bc5
Maxim <mihalenko.maxim21@gmail.com>
2023-03-16 19:09:43 +0300

    Add redux-toolkit and bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "src/App";
+import { store } from "src/store";
 import "src/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
