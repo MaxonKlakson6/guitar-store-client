761b43fc4f97eb5ca58d086d1bddad36f5ea546a
Maxim <mihalenko.maxim21@gmail.com>
2023-04-20 20:22:18 +0300

    add no items message

--- src/pages/Store/components/StoreLayout/index.tsx
+++ src/pages/Store/components/StoreLayout/index.tsx
@@ -14,10 +14,14 @@ const StoreLayout = ({ category, products, loadGoods }: StoreLayoutProps) => {
   return (
     <div className={styleClasses.wrapper}>
       <Filter category={category} loadGoods={loadGoods} />
+      {products.guitars.length === 0 && products.accessories.length === 0 && (
+        <h1>Ничего не найдено 😢</h1>
+      )}
       <div className={styleClasses.goodsHolder}>
         {products.guitars.map((product) => (
           <ProductCard
             key={product.id}
+            productId={product.id}
             productName={product.name}
             price={product.price}
             image={product.image}
@@ -26,6 +30,7 @@ const StoreLayout = ({ category, products, loadGoods }: StoreLayoutProps) => {
         {products.accessories.map((accessory) => (
           <ProductCard
             key={accessory.id}
+            productId={accessory.id}
             productName={accessory.name}
             price={accessory.price}
             image={accessory.image}
